308f5ad09c6ba697dcf73c1c991b3740e562f899
Alexander Papchikhin <aleshmandr@gmail.com>
2021-11-22 00:30:36 +0100

    Token renamed

--- contracts/MyHyperverseToken.sol
+++ contracts/MyHyperverseToken.sol
@@ -2,9 +2,16 @@ pragma solidity ^0.8.10;
 
 contract MyHyperverseToken {
     
+    string public name = "My Hyperverse Token";
+    string public symbol = "MHV";
+    string public standard = "My Hyperverse Token v1.0";
+
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceOf;
+
     constructor(uint256 _totalSupply) {
+        balanceOf[msg.sender] = _totalSupply;
         totalSupply = _totalSupply;
     }
 }
--- test/MyHyperverseToken.js
+++ test/MyHyperverseToken.js
@@ -1,12 +1,34 @@
 ﻿var MyHyperverseToken = artifacts.require("./MyHyperverseToken.sol");
 
 contract('MyHyperverse', function (accounts){
+
+    var initialSupply = 1000000;
+    var tokenInstance;
+
+    it('initialize  contract with correct values', function(){
+        return MyHyperverseToken.deployed().then(function (instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(tokenName){
+            assert.equal(tokenName, "My Hyperverse Token", 'has correct name');
+            return tokenInstance.symbol();
+        }).then(function(tokenSymbol){
+            assert.equal(tokenSymbol, "MHV", 'has correct symbol');
+            return tokenInstance.standard();
+        }).then(function(tokenStandatd){
+            assert.equal(tokenStandatd, "My Hyperverse Token v1.0", 'has correct standard');
+        });
+    });
+
     it('set total supply upon deployment ', function () {
         return MyHyperverseToken.deployed().then(function (instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function (totalSupply){
-            assert.equal(totalSupply.toNumber(), 1000000, 'sets total supply to 1,000,000'); 
+            assert.equal(totalSupply.toNumber(), initialSupply, 'sets total supply to 1,000,000');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance){
+            assert.equal(adminBalance.toNumber(), initialSupply, 'allocates initial supply to admin')
         });
     });
 })
