f5fd6cff0a4c825090bdf0686cfa08aca079e762
Alexander Papchikhin <aleshmandr@gmail.com>
2021-12-04 16:31:50 +0100

    Buyng tokens on sale

--- contracts/MyHyperverseTokenSale.sol
+++ contracts/MyHyperverseTokenSale.sol
@@ -5,6 +5,7 @@ import "./MyHyperverseToken.sol";
 contract MyHyperverseTokenSale {
     MyHyperverseToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
 
     address admin;
 
@@ -13,4 +14,21 @@ contract MyHyperverseTokenSale {
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
+
+    function buyTokens(uint256 numberOfTokens) public payable{
+        require(msg.value == multiply(numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= numberOfTokens);
+        require(tokenContract.transfer(msg.sender, numberOfTokens));
+        tokensSold+=numberOfTokens;
+        emit Sell(msg.sender, numberOfTokens);
+    }
+
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z){
+        require(y == 0 || (z = x * y ) / y == x, "multiply overflow");
+    }
+
+    event Sell(
+        address indexed buyer,
+        uint256 amount
+    );
 }
--- test/MyHuperverseTokenSale.js
+++ test/MyHuperverseTokenSale.js
@@ -1,8 +1,14 @@
 var MyHyperverseTokenSale = artifacts.require("./MyHyperverseTokenSale.sol");
+var MyHyperverseToken = artifacts.require("./MyHyperverseToken.sol");
 
 contract('MyHyperverseSale', function (accounts){
     var tokenSaleInstance;
-    var correctTokenPrice = 10000000000000000;//0.001 eth
+    var tokenInstance;
+    const correctTokenPrice = 10000000000000000;//0.001 eth
+    const tokenBuyer = accounts[2];
+    const admin = accounts[0];
+    const numberOfTokensToBuy = 1000;
+    const tokensToSale = 300000;// 30% of total supply
 
     it("initializes the contract with correct values", async ()=>{
         tokenSaleInstance = await MyHyperverseTokenSale.deployed();
@@ -12,5 +18,40 @@ contract('MyHyperverseSale', function (accounts){
         assert.notEqual(tokenAddress, "0x0", "has a token contact address");
         const tokenPrice = await tokenSaleInstance.tokenPrice();
         assert.equal(tokenPrice, correctTokenPrice, "token price is correct");
+    });
+
+    it("facilitates token buying", async ()=>{
+        tokenInstance = await MyHyperverseToken.deployed();
+        tokenSaleInstance = await MyHyperverseTokenSale.deployed();
+        const address = tokenSaleInstance.address;
+        await tokenInstance.transfer(tokenSaleInstance.address, tokensToSale, {from: admin});
+
+        var value = numberOfTokensToBuy * correctTokenPrice;
+        var receipt = await tokenSaleInstance.buyTokens(numberOfTokensToBuy, {from: tokenBuyer, value: value});
+        assert.equal(receipt.logs.length, 1, "has one event");
+        assert.equal(receipt.logs[0].event, "Sell", "has Sell event");
+        assert.equal(receipt.logs[0].args.buyer, tokenBuyer, "Sell event has buyer account");
+        assert.equal(receipt.logs[0].args.amount, numberOfTokensToBuy, "Sell event has tokens amount data ");
+
+        var balanceOfBuyer = await tokenInstance.balanceOf(tokenBuyer);
+        assert.equal(balanceOfBuyer.toNumber(), numberOfTokensToBuy, "increases balance of buyer");
+        assert.equal((await tokenInstance.balanceOf(address)).toNumber(), tokensToSale - numberOfTokensToBuy, "decreases balance of sale");
+
+        const tokensSold = await tokenSaleInstance.tokensSold();
+        assert.equal(tokensSold.toNumber(), numberOfTokensToBuy, "increments number of tokens sold")
+        try{
+            await tokenSaleInstance.buyTokens(numberOfTokensToBuy, {from: tokenBuyer, value: 1});
+            assert.fail();
+        }catch (error) {
+            assert(error.message.indexOf('revert')>=0, "Sell message must contain revert");
+        }
+        
+        try{
+            await tokenSaleInstance.buyTokens(300001, {from: tokenBuyer, value: value});
+            assert.fail();
+        }catch (error) {
+            assert(error.message.indexOf('revert')>=0, "rejects buying more tokens than available");
+        }
+
     });
 });
